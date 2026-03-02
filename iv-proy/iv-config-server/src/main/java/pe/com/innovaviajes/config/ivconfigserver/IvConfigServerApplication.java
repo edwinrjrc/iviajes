63df1d52093fa4543c4243c1c12f02c40bd145ad
edwinrjrc <edwinrjrc@gmail.com>
2026-03-02 14:57:37 -0500

    Nuevo servicio de config server

---
+++ iv-proy/iv-config-server/src/main/java/pe/com/innovaviajes/config/ivconfigserver/IvConfigServerApplication.java
@@ -0,0 +1,13 @@
+package pe.com.innovaviajes.config.ivconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvConfigServerApplication.class, args);
+	}
+
+}
---
+++ iv-proy/iv-config-server/src/test/java/pe/com/innovaviajes/config/ivconfigserver/IvConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.innovaviajes.config.ivconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
