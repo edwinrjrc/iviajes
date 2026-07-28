0312086d353e1c142042930400e81bd2dfff5540
edwinrjrc <edwinrjrc@gmail.com>
2026-07-28 11:59:53 -0500

    implementacion de docker

---
+++ iv-proy/iv-service-hoteles/src/main/java/pe/com/innovaviajes/web/ivservicehoteles/IvServiceHotelesApplication.java
@@ -0,0 +1,11 @@
+package pe.com.innovaviajes.web.ivservicehoteles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvServiceHotelesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IvServiceHotelesApplication.class, args);
+    }
+}
